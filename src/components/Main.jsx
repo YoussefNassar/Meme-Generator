0064c879d958612f438677c1d239471a8a2af5d2
Youssef Nassar <youssef.nassar@solactive.com>
2025-03-01 20:49:06 +0100

    implement changing images

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,6 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
-
     const [allMemes, setAllMemes] = useState([])
 
     useEffect(() => {
@@ -14,6 +13,15 @@ export default function Main() {
         .then(res => res.json())
         .then(data => setAllMemes(data.data.memes))
     }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: newMemeUrl
+        }))
+    }
 
     function handleChange(event) {
         const {value, name} = event.currentTarget
@@ -45,7 +53,7 @@ export default function Main() {
                         value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageUrl} />
